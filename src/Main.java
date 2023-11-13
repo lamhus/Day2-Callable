896ce48ba9ab7eb603ceeb2e3f84b282f410cf66
lamhus <lam.hus713@gmail.com>
2023-11-13 14:20:15 -0500

    first
    write code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        int count=0;
+        for(int i=1; i<=1000000;i++)
+            count++;
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        int num=100000000;
+        int numThreads=100;
+        int numbersPThread= num/numThreads;
+        ExecutorService executorService= Executors.newFixedThreadPool(100);
+        List<Future<Integer>> futures=new ArrayList<>();
+
+        for(int i=0;i<100;i++)
+        {
+           
+            Callable<Integer> task= new Main();
+            Future<Integer> future= executorService.submit(task);
+            futures.add(future);
+        }
+
+        int total=0;
+        for(Future<Integer>future: futures)
+        {
+            try{
+                total+=future.get();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        executorService.shutdown();
+        System.out.println("Total count: "+total);
     }
 }
